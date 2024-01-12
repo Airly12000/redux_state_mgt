549255a94f1aa0eccca4e28813d1b5978670f4fe
Airly12000 <littlefrancis28@gmail.com>
2024-01-12 02:12:20 +0000

    messages setup

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
-import { setIsLoggedIn } from "../redux/userSlice";
-import { useNavigate } from "react-router-dom";
+import { endpoints } from "../utils/Endpoints";
+import Requests from "../utils/Requests";
+import { setStoreValues } from "../redux/userSlice";
+
+const errorHandler = (error: any) => console.log(`Error: ${error}`);
 
 const Dashboard = () => {
+  const messages = useSelector((state: RootState) => state.messages);
+  const dispatch = useDispatch<AppDispatch>();
+  const [message, setMessage] = useState("");
+
+  const getMessages = async () => {
+    await Requests.get(endpoints.messages)
+      .then((response) => {
+        dispatch(
+          setStoreValues({ key: "messages", value: response.data.messages })
+        );
+      })
+      .catch(errorHandler);
+  };
+
+  const handleAddMessage = async (event: any) => {
+    event.preventDefault();
+    await Requests.post(endpoints.messages + "/post")
+      .then((response) => {})
+      .catch(errorHandler);
+  };
+
+  useEffect(() => {
+    // getMessages();
+  });
+
   return (
     <div className="col">
       <h1>Dashboard</h1>
+      <div className="row border p-3">
+        <div className="col">
+          {messages &&
+            messages.map((message: any, index: any) => {
+              return (
+                <div className="col" key={index}>
+                  {message}
+                </div>
+              );
+            })}
+        </div>
+        <div className="col d-flex flex-column">
+          <input
+            type="text"
+            className="form-control mb-3"
+            placeholder="type message..."
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+          />
+          <button
+            className="btn btn-primary align-self-center"
+            onClick={handleAddMessage}
+            type="submit"
+          >
+            Add New Message
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
