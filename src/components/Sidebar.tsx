94a6b681f74fcb0d9444c1f21a0d5c609ace886a
Airly12000 <littlefrancis28@gmail.com>
2024-01-12 02:10:41 +0000

    logout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
 import { setIsLoggedIn } from "../redux/userSlice";
+import Requests from "../utils/Requests";
+import { endpoints } from "../utils/Endpoints";
 
 const Sidebar = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const { refreshToken } = useSelector((state: RootState) => state.user);
+
+  const handleLogout = async () => {
+    await Requests.post(endpoints.logout, { refreshToken: refreshToken });
+    dispatch(setIsLoggedIn(false));
+  };
+
   return (
     <div className="d-none d-md-flex col-2 border justify-content-evenly flex-column align-items-center">
       <div className="col">Sidebar</div>
       <div className="col d-flex flex-column-reverse">
-        <button onClick={() => dispatch(setIsLoggedIn(false))}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
